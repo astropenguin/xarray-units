f12eb6ec50a99f4555fbec9192e1163d10c15c7f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-11 12:59:04 +0000

    #3 Add package documentation

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# project information
+author = "Akio Taniguchi"
+copyright = "2023 Akio Taniguchi"
+
+
+# general configuration
+add_module_names = False
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+myst_heading_anchors = 3
+templates_path = ["_templates"]
+
+
+# options for HTML output
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/astropenguin/xarray-units/",
+    "logo": {"text": "xarray-units"},
+}
