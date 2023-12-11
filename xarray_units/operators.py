ae80ad7eba9e903122a7e661acc2bc229a1398f1
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-11 14:00:14 +0000

    #5 Add type hints for supported operators

--- xarray_units/operators.py
+++ xarray_units/operators.py
@@ -1,1 +1,24 @@
 __all__ = []
+
+
+# standard library
+from typing import Literal
+
+
+# type hints
+Operator = Literal[
+    "lt",  # <
+    "le",  # <=
+    "eq",  # ==
+    "ne",  # !=
+    "ge",  # >=
+    "gt",  # >
+    "add",  # +
+    "sub",  # -
+    "mul",  # *
+    "pow",  # **
+    "matmul",  # @
+    "truediv",  # /
+    "floordiv",  # //
+    "mod",  # %
+]
