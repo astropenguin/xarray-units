9e6d1709e828b3394d05140b1ec3e07ef1101e26
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-17 13:58:10 +0000

    #5 Rename module (operators → operator)

--- xarray_units/__init__.py
+++ xarray_units/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ["operators", "quantity", "utils"]
+__all__ = ["operator", "quantity", "utils"]
 __version__ = "0.1.0"
 
 
 # submodules
-from . import operators
+from . import operator
 from . import quantity
 from . import utils
