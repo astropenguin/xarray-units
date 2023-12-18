9164d072bee19b746bf130fa0e9508926b9ce22c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-18 18:23:07 +0000

    #6 Add initial accessor module

--- xarray_units/__init__.py
+++ xarray_units/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ["operator", "quantity", "utils"]
+__all__ = ["accessor", "operator", "quantity", "utils"]
 __version__ = "0.2.0"
 
 
 # submodules
+from . import accessor
 from . import operator
 from . import quantity
 from . import utils
