3184b9191693f62961bd7a40f34a74e82fafbb05
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-19 17:22:27 +0000

    #6 Add operator functions to accessor

--- xarray_units/accessor.py
+++ xarray_units/accessor.py
@@ -9,7 +9,7 @@ from typing import Callable, Generic
 
 # dependencies
 from typing_extensions import Concatenate, ParamSpec
-from . import quantity
+from . import operator, quantity
 from .utils import TDataArray
 
 
@@ -40,10 +40,43 @@ class Units(Generic[TDataArray]):
     accessed: TDataArray
     """DataArray to be accessed."""
 
-    # quantity
+    # quantity module
     apply = to_method(quantity.apply)
     decompose = to_method(quantity.decompose)
     like = to_method(quantity.like)
     set = to_method(quantity.set)
     to = to_method(quantity.to)
     unset = to_method(quantity.unset)
+
+    # operator module
+    take = to_method(operator.take)
+    mul = to_method(operator.mul)
+    pow = to_method(operator.pow)
+    matmul = to_method(operator.matmul)
+    truediv = to_method(operator.truediv)
+    add = to_method(operator.add)
+    sub = to_method(operator.sub)
+    floordiv = to_method(operator.floordiv)
+    mod = to_method(operator.mod)
+    lt = to_method(operator.lt)
+    le = to_method(operator.le)
+    eq = to_method(operator.eq)
+    ne = to_method(operator.ne)
+    ge = to_method(operator.ge)
+    gt = to_method(operator.gt)
+
+    # aliases
+    __mul__ = mul
+    __pow__ = pow
+    __matmul__ = matmul
+    __truediv__ = truediv
+    __add__ = add
+    __sub__ = sub
+    __floordiv__ = floordiv
+    __mod__ = mod
+    __lt__ = lt
+    __le__ = le
+    __eq__ = eq  # type: ignore
+    __ne__ = ne  # type: ignore
+    __ge__ = ge
+    __gt__ = gt
