fa25ecb16c5fc8f33b731f4ef0391107c1d61147
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-12-19 16:55:55 +0000

    #6 Add DataArray accessor for handling units

--- xarray_units/accessor.py
+++ xarray_units/accessor.py
@@ -1,1 +1,40 @@
-__all__ = []
+__all__ = ["Units"]
+
+
+# standard library
+from dataclasses import dataclass
+from functools import wraps
+from typing import Callable, Generic
+
+
+# dependencies
+from typing_extensions import Concatenate, ParamSpec
+from .utils import TDataArray
+
+
+# type hints
+P = ParamSpec("P")
+
+
+def to_method(
+    func: Callable[Concatenate[TDataArray, P], TDataArray]
+) -> Callable[Concatenate["Units[TDataArray]", P], TDataArray]:
+    """Convert an operator or quantity function to a method."""
+
+    @wraps(func)
+    def wrapper(
+        units: "Units[TDataArray]",
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> TDataArray:
+        return func(units.accessed, *args, **kwargs)
+
+    return wrapper
+
+
+@dataclass
+class Units(Generic[TDataArray]):
+    """DataArray accessor for handling units."""
+
+    accessed: TDataArray
+    """DataArray to be accessed."""
